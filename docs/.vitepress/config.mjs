725daa6ca56c31570395ea406dd0b3dabc754522
kurimiaisu <96464216+kurimiaisu@users.noreply.github.com>
2026-02-27 01:20:08 +0300

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  srcDir: "..\\redshape",
+  srcDir: "",
   
   title: "RedShape",
   description: "Сайт с лором редшейпа",
